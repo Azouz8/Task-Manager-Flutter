a5b3aa3388aa9012c2d35bb3b90e9b59745a025f
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2026-06-28 15:21:02 +0300

    Refactored MatrixTaskCard widget to make each task card a draggable widget

--- lib/widgets/matrix/matrix_task_card.dart
+++ lib/widgets/matrix/matrix_task_card.dart
@@ -1,41 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/models/task_model.dart';
 import 'package:task_manager/widgets/custom_checkbox.dart';
-
 class MatrixTaskCard extends StatelessWidget {
   const MatrixTaskCard({
     super.key,
-    required this.taskTitle,
-    required this.color,
+    required this.task,
   });
-  final String taskTitle;
-  final Color color;
+
+  final TaskModel task;
 
   @override
   Widget build(BuildContext context) {
+    return Draggable<TaskModel>(
+      data: task,
+      feedback: Material(
+        type: MaterialType.transparency,
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.4,
+          child: _buildBaseContainer(context),
+        ),
+      ),
+      childWhenDragging: Opacity(
+        opacity: 0.4,
+        child: _buildBaseContainer(context),
+      ),
+      child: _buildBaseContainer(context),
+    );
+  }
+
+  Container _buildBaseContainer(BuildContext context){
     return Container(
       margin: const EdgeInsets.only(bottom: 10),
-      padding: const EdgeInsets.symmetric(
-        horizontal: 10,
-        vertical: 14,
-      ),
+      padding: const EdgeInsets.all(8),
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.secondary,
         borderRadius: BorderRadius.circular(12),
       ),
       child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          CustomCheckbox(color: color),
+          CustomCheckbox(task: task),
           const SizedBox(width: 12),
           Expanded(
             child: Text(
-              taskTitle,
+              task.title,
               style: const TextStyle(
                 color: Colors.white,
                 fontSize: 12,
                 fontWeight: FontWeight.w500,
               ),
-              maxLines: 3,
+              maxLines: 1,
               overflow: TextOverflow.ellipsis,
             ),
           ),
