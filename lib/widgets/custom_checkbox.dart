d605711bd4f0fb6d73ad4d14bdd6ee456523b100
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2026-06-28 15:22:33 +0300

    Refactored the CustomCheckbox widget to use the Material design instead of custom design

--- lib/widgets/custom_checkbox.dart
+++ lib/widgets/custom_checkbox.dart
@@ -1,22 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_manager/cubits/task_cubit/task_cubit.dart';
+import 'package:task_manager/models/task_model.dart';
 
-class CustomCheckbox extends StatelessWidget {
-  const CustomCheckbox({super.key, required this.color});
+class CustomCheckbox extends StatefulWidget {
+  const CustomCheckbox({super.key, required this.task});
 
-  final Color color;
+  final TaskModel task;
+
+  @override
+  State<CustomCheckbox> createState() => _CustomCheckboxState();
+}
+
+class _CustomCheckboxState extends State<CustomCheckbox> {
+  late bool isDone;
+
+  @override
+  void initState() {
+    super.initState();
+    isDone = widget.task.status == TaskStatus.done;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 15,
-      height: 15,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: color,
-          width: 1.5,
-        ),
-        borderRadius: BorderRadius.circular(2),
-      ),
+    return Checkbox(
+      value: isDone,
+      onChanged: _onChanged,
+      side: BorderSide(color: widget.task.category.color),
+      activeColor: widget.task.category.color,
+      visualDensity: VisualDensity.compact,
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+    );
+  }
+
+  void _onChanged(bool? value) {
+    setState(() {
+      isDone = value ?? false;
+    });
+    context.read<TaskCubit>().updateTask(
+      widget.task.copyWith(status: isDone ? TaskStatus.done : TaskStatus.todo),
     );
   }
 }
