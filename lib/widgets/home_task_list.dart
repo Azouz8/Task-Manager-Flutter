fdda7f27f07bd4a6c9ee2729850dc1cbda981e5d
Ali Azouz <aliazouz.808@gmail.com>
2026-06-28 20:18:38 +0300

    Fixing Home Issues

--- lib/widgets/home_task_list.dart
+++ lib/widgets/home_task_list.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:task_manager/cubits/home_cubit/home_cubit.dart';
 import 'package:task_manager/cubits/task_cubit/task_cubit.dart';
-import 'package:task_manager/cubits/task_cubit/tast_states.dart';
+import 'package:task_manager/cubits/task_cubit/task_states.dart';
 import 'package:task_manager/widgets/task_card.dart';
 
 class HomeTaskList extends StatelessWidget {
@@ -18,20 +18,41 @@ class HomeTaskList extends StatelessWidget {
             final allTasks = taskContext.read<TaskCubit>().getTasksByStatus(
               homeState.selectedControl,
             );
-            return Expanded(
-              child: ListView.builder(
-                physics: const BouncingScrollPhysics(),
-                itemCount: allTasks.length,
-                itemBuilder: (context, index) {
-                  final task = allTasks[index];
-                  return TaskCard(
-                    allTasks[index],
-                    key: ValueKey(task.id),
-                    updateTask: updateTask,
+            return allTasks.isNotEmpty
+                ? Expanded(
+                    child: ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      itemCount: allTasks.length,
+                      itemBuilder: (context, index) {
+                        final task = allTasks[index];
+                        return TaskCard(
+                          allTasks[index],
+                          key: ValueKey(task.id),
+                          updateTask: updateTask,
+                        );
+                      },
+                    ),
+                  )
+                : Expanded(
+                    child: Column(
+                      mainAxisAlignment: .center,
+                      mainAxisSize: .min,
+                      children: [
+                        Text(
+                          "There is no Tasks to Do,",
+                          style: Theme.of(
+                            context,
+                          ).textTheme.bodyMedium!.copyWith(color: Colors.grey),
+                        ),
+                        Text(
+                          "Hurry up and add some!",
+                          style: Theme.of(
+                            context,
+                          ).textTheme.bodyMedium!.copyWith(color: Colors.grey),
+                        ),
+                      ],
+                    ),
                   );
-                },
-              ),
-            );
           },
         );
       },
